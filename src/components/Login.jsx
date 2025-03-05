9d9205150476be7bea25791448aecdbfbff08243
devon1910 <ekpodave2000@gmail.com>
2025-03-05 11:06:51 +0100

    Added icon to hide/show passwords

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,50 +1,82 @@
-import React, { useState } from 'react'
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
-import { loginUser } from '../services/swiftlineService';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../services/swiftlineService";
+import { Button, InputGroup } from "react-bootstrap";
+import { Eye, EyeSlashFill } from "react-bootstrap-icons";
 
 const Login = () => {
-  const [email,setEmail] = useState("")
-  const [password,setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigator = useNavigate();
   function handleLogin(e) {
     e.preventDefault();
-    const loginRequest= {email,password};
+    const loginRequest = { email, password };
     loginUser(loginRequest)
       .then((response) => {
-        console.log("response: ",response);
+        console.log("response: ", response);
         navigator("/dashboard");
       })
       .catch((error) => {
-        const errormsg= error.response.data.errors.join();
-        console.log("error: ",error);
+        const errormsg = error.response.data.errors.join();
+        console.log("error: ", error);
         alert(`${errormsg}` || error.response.data.errormsg);
-      }); 
+      });
   }
   return (
     <div>
+      <Form onSubmit={handleLogin}>
         <h3>Login</h3>
-        <form>
-        <FloatingLabel
-            controlId="floatingEmail"
-            label="Email address"
-            className="mb-3">
-            <Form.Control type="email" placeholder="name@example.com" onChange={(e) => setEmail(e.target.value)} />
-        </FloatingLabel> 
-        <FloatingLabel
-            controlId="floatingPass"
-            label="Password"
-            className="mb-3">
-            <Form.Control type="password" placeholder="" onChange={(e) => setPassword(e.target.value)} />
-        </FloatingLabel>
-            <button type="submit" className="btn btn-primary mt-3" onClick={handleLogin}>
-            Login
-            </button>
-        </form>
-      </div>
-  )
-}
 
-export default Login
+        <Form.Group className="mb-3">
+          <Form.Floating>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              id="floatingEmail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="floatingEmail">Email address</label>
+          </Form.Floating>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Floating>
+            <Form.Control
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              id="floatingPassword"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="floatingPassword">Password</label>
+            <InputGroup.Text
+              onClick={() => setShowPassword(!showPassword)}
+              style={{ 
+                position: 'absolute', 
+                right: '10px', 
+                top: '50%', 
+                transform: 'translateY(-50%)', 
+                cursor: "pointer", 
+                zIndex: 5 
+              }}>
+              {showPassword ? <EyeSlashFill /> : <Eye />}
+            </InputGroup.Text>
+          </Form.Floating>
+        </Form.Group>
+
+        <Button 
+          type="submit" 
+          variant="primary" 
+          className="mt-3">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-import { FloatingLabel } from "react-bootstrap";
+import { Button, InputGroup } from "react-bootstrap";
+import { Eye, EyeSlashFill } from "react-bootstrap-icons";
 import Form from "react-bootstrap/Form";
 
 const SignUp = () => {
+  const [email, setEmail]= useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -20,46 +25,83 @@ const SignUp = () => {
 
   return (
     <div>
+      <Form onSubmit={handleSubmit}>
       <h3>Sign Up</h3>
-      <form>
-        <FloatingLabel
-          controlId="floatingEmailSignUp"
-          label="Email address"
-          className="mb-3"
-        >
-          <Form.Control type="text" placeholder="Email" />
-        </FloatingLabel>
-        <FloatingLabel
-          controlId="floatingPassSignUp"
-          label="Password"
-          className="mb-3"
-        >
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </FloatingLabel>
-        <FloatingLabel
-          controlId="floatingConfirmPass"
-          label="Confirm Password"
-          className="mb-3"
-        >
-          <Form.Control
-            type="password"
-            placeholder="Confirm Password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </FloatingLabel>
 
-        <button
+        <Form.Group className="mb-3">
+          <Form.Floating>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              id="floatingEmail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="floatingEmail">Email address</label>
+          </Form.Floating>
+        </Form.Group>
+
+        
+        <Form.Group className="mb-3">
+          <Form.Floating>
+            <Form.Control
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              id="floatingPassword"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="floatingPassword">Password</label>
+            <InputGroup.Text
+              onClick={() => setShowPassword(!showPassword)}
+              style={{ 
+                position: 'absolute', 
+                right: '10px', 
+                top: '50%', 
+                transform: 'translateY(-50%)', 
+                cursor: "pointer", 
+                zIndex: 5 
+              }}
+            >
+              {showPassword ? <EyeSlashFill /> : <Eye />}
+            </InputGroup.Text>
+          </Form.Floating>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Floating>
+            <Form.Control
+              type={showPassword ? "text" : "password"}
+              placeholder=" Confirm Password"
+              id="floatingConfirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <label htmlFor="floatingConfirmPassword">Confirm Password</label>
+            <InputGroup.Text
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              style={{ 
+                position: 'absolute', 
+                right: '10px', 
+                top: '50%', 
+                transform: 'translateY(-50%)', 
+                cursor: "pointer", 
+                zIndex: 5 
+              }}
+            >
+              {showConfirmPassword ? <EyeSlashFill /> : <Eye />}
+            </InputGroup.Text>
+          </Form.Floating>
+        </Form.Group>
+
+        <Button
           type="submit"
           className="btn btn-primary mt-3"
           onClick={handleSubmit}
         >
           Sign Up
-        </button>
-      </form>
+        </Button>
+      </Form>  
     </div>
   );
 };
