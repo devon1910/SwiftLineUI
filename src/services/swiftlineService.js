385c8846d1f907d9e7ed70ed6bd72bf1b68b3c70
devon1910 <ekpodave2000@gmail.com>
2025-04-04 22:13:44 +0100

    Refactored Service layer

--- src/services/swiftlineService.js
+++ src/services/swiftlineService.js
@@ -4,38 +4,21 @@ import API from "./APIService";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
-export const eventsList = (page, eventsPerPage, search="") =>
-  API.get(apiUrl + "Event/SearchEvents?page="+page+"&size="+eventsPerPage+"&query="+search);
-
-export const createEvent = (event) =>
-  API.post(apiUrl + "Event/CreateEvent", event);
-
-export const eventQueueInfo = (eventId) =>
-  API.get(apiUrl + "Event/GetEventQueue/" + eventId);
-
-export const UserEvents = () => API.get(apiUrl + "Event/GetUserEvents");
-export const updateEvent = (event) =>
-  API.put(apiUrl + "Event/EditEvent", event);
-
-export const joinLine = (eventId) =>
-  API.post(apiUrl + "Event/JoinEvent?EventId=" + eventId);
-
-export const validateToken = (token) =>
-  API.post(apiUrl + "Auth/VerifyToken?token=" + token);
-export const deleteEvent = (id) =>
-  API.delete(apiUrl + "Event/DeleteEvent/"+id);
-
-export const lineInfo = function (queue) {
-  const lineMemberId = queue.lineMemberId;
-  return API.get(apiUrl + "Line/GetLineInfo/" + lineMemberId);
-};
-
-export const GetUserLineInfo = () => API.get(apiUrl + "Line/GetUserLineInfo");
-
-export const fetchEventById = (eventId) =>
-  API.get(apiUrl + "Event/GetEvent/" + eventId);
-
+//Auth
+export const validateToken = (token) => API.post(apiUrl + "Auth/VerifyToken?token=" + token);
 export const loginUser= (loginRequest) => API.post(apiUrl + "Auth/Login", loginRequest);
 export const SignUpUser= (SignUpRequest) => API.post(apiUrl + "Auth/SignUp", SignUpRequest);
 export const LogOut= () => API.post(apiUrl + "Auth/Revoke");
+
+//Events
+export const eventsList = (page, eventsPerPage, search="") => API.get(apiUrl + "Event/SearchEvents?page="+page+"&size="+eventsPerPage+"&query="+search);
+export const createEvent = (event) => API.post(apiUrl + "Event/CreateEvent", event);
+export const eventQueueInfo = (eventId) => API.get(apiUrl + "Event/GetEventQueue/" + eventId);
+export const UserEvents = () => API.get(apiUrl + "Event/GetUserEvents");
+export const updateEvent = (event) => API.put(apiUrl + "Event/EditEvent", event);
+export const deleteEvent = (id) => API.delete(apiUrl + "Event/DeleteEvent/"+id);
+export const fetchEventById = (eventId) => API.get(apiUrl + "Event/GetEvent/" + eventId);
+
+//Lines
+export const GetUserLineInfo = () => API.get(apiUrl + "Line/GetUserLineInfo");
 
