b20e6cf9794c46a25df9db0aeae30e85dcfba95d
devon1910 <ekpodave2000@gmail.com>
2025-04-24 13:21:50 +0100

    Changes to enable straight sign up

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -21,6 +21,7 @@ const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [isLoading, setIsLoading] = useState(false)
+  const from = location.state?.from || null;
   const [isTurnstileVerified, setIsTurnstileVerified] = useState(false);
   const handleGoogleSignIn = async () => {
     // if (!isTurnstileVerified) {
@@ -59,7 +60,20 @@ const SignUp = () => {
           setIsLoading(false)
         }
         else{
-          setIsFormSubmitted(true);
+          //setIsFormSubmitted(true); to show instruction message for verification
+          if (from) {
+            window.location.href = from;
+          } else {
+            navigator("/", {
+              state: {
+                email: response.data.data.email,
+                isInLine: response.data.data.isInLine,
+                userId: response.data.data.userId,
+                userName: response.data.data.userName,
+              },
+              replace: true,
+            });
+          }
           setIsLoading(false)
         }
       })
