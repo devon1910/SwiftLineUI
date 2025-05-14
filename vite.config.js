f50cc83acacd52d9e4794e8cef2fb8099163698c
devon1910 <ekpodave2000@gmail.com>
2025-05-14 23:06:17 +0100

    Trying to fix PWA bug

--- vite.config.js
+++ vite.config.js
@@ -3,49 +3,69 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from "vite-plugin-pwa";
 
-const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
-  manifest:{
-    name:"Swiftline",
-    short_name:"swiftline",
-    description:"A queue management system that allows users to create and manage events, view their queue status",
-    icons:[{
+const manifestForPlugin = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Swiftline",
+    short_name: "swiftline",
+    description: "A queue management system that allows users to create and manage events, view their queue status",
+    icons: [{
       src: '/android-chrome-192x192.png',
-      sizes:'192x192',
-      type:'image/png',
-      purpose:'favicon'
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'any'
     },
     {
-      src:'/android-chrome-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'favicon'
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any'
     },
     {
       src: '/apple-touch-icon.png',
-      sizes:'180x180',
-      type:'image/png',
-      purpose:'apple touch icon',
+      sizes: '180x180',
+      type: 'image/png',
+      purpose: 'any'
     },
     {
       src: '/maskable_icon.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable',
-    }
-  ],
-  theme_color:'#698474',
-  background_color:'#698474',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable'
+    }],
+    theme_color: '#698474',
+    background_color: '#698474',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
+  },
+  devOptions: {
+    enabled: true
+  },
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'google-fonts-cache',
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
   }
 }
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),VitePWA(manifestForPlugIn)], 
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugin)], 
 })
