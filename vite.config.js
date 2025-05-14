1346d7ef27c37a3cc51921f8a2f3bd9ce2eaf613
devon1910 <ekpodave2000@gmail.com>
2025-05-14 22:43:17 +0100

    Made app PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Swiftline",
+    short_name:"swiftline",
+    description:"A queue management system that allows users to create and manage events, view their queue status",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#698474',
+  background_color:'#698474',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),], 
+  plugins: [react(), tailwindcss(),VitePWA(manifestForPlugIn)], 
 })
